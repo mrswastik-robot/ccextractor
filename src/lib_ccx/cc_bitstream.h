af1c7e48da1e8710b8dfaadf9bd627629f6d43d7
Swastik Patel <swastikpatel29@gmail.com>
2024-12-25 18:16:10 +0530

    feat: Add bitstream module

--- src/lib_ccx/cc_bitstream.h
+++ src/lib_ccx/cc_bitstream.h
@@ -24,6 +24,9 @@ struct bitstream
 	// increased by the calling function, or not.
 	unsigned char *_i_pos;
 	int _i_bpos;
+#ifndef DISABLE_RUST
+	void *rust_bs;  // Pointer to Rust's Bitstream struct
+#endif
 };
 
 #define read_u8(bstream) (uint8_t)bitstream_get_num(bstream,1,1)
